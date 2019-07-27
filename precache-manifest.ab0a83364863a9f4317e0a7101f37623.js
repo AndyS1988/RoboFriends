5a9f0cbf4983199eb3089ea6f54395f4dec392f7
AndyS1988 <spaska.andrea@gmail.com>
2019-07-26 18:47:54 -0700

    Updates

---
+++ precache-manifest.ab0a83364863a9f4317e0a7101f37623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "798d9e0a60f3b285a14c",
+    "url": "/RoboFriends/static/js/main.798d9e0a.chunk.js"
+  },
+  {
+    "revision": "059be5efdb4059ed0c73",
+    "url": "/RoboFriends/static/js/1.059be5ef.chunk.js"
+  },
+  {
+    "revision": "798d9e0a60f3b285a14c",
+    "url": "/RoboFriends/static/css/main.af2c1b2c.chunk.css"
+  },
+  {
+    "revision": "5c84357fcae5f487e5e83635a162d87a",
+    "url": "/RoboFriends/index.html"
+  }
+];
